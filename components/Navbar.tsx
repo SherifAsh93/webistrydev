666f85c21dc9c0a55288ac293cb61b4043e8673e
Sherif <sherif.ash93@gmail.com>
2026-08-18 02:06:00 +0300

    Updated main site subtitle

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -56,7 +56,7 @@ export default function Navbar() {
               Webistry<span className="text-gradient">dev</span>
             </span>
             <span className="text-[9px] font-bold text-slate-400 uppercase tracking-[0.18em]">
-              Full-Stack Developer
+              Custom Software Solutions for Business Owners
             </span>
           </div>
         </button>
